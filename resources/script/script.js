e9ac652963c91ffb4abc983517c830508e0a2e44
GrimmReaper1O1 <152280801+GrimmReaper1O1@users.noreply.github.com>
2025-02-28 22:02:31 +1100

    Add files via upload

--- resources/script/script.js
+++ resources/script/script.js
@@ -21,7 +21,7 @@ const individuals = {
         
     },
     rGen: function() {
-        let value = (Math.floor(Math.random() * (this._durationSixteen -3) +3)); 
+        let value = (Math.floor(Math.random() * (this._durationSixteen -2) +3)); 
         return value;
     },
     removeTime: function(time) {
@@ -43,7 +43,6 @@ const individuals = {
             this._names[this._count].gender = 'male';
             this._names[this._count].durationOfLIfe = rando;
             this._names[this._count].age = age;
-            this._names[this._count].length = this._count;
             this._countm++;
         } else if (truthy) {
             this._names[this._count] = {};
@@ -51,7 +50,6 @@ const individuals = {
             this._names[this._count].gender = 'female';
             this._names[this._count].durationOfLIfe = rando;
             this._names[this._count].age = age;
-            this._names[this._count].length = this._count;
             this._countf++;
         }
         this._count++;
@@ -84,16 +82,21 @@ const individuals = {
     let mtwelve = 0;
     let msixteen = 0;
     let mcount = 0
+    let kcount = 0;
     for (let i = 0; i < individuals._names.length ; i++) {
         console.log(i);
         if (individuals._names[i].durationOfLIfe <= 4) {
             individuals._names[i].gender === 'male' ? mfour++ : ffour++;
+            kcount++;
         } else if (individuals._names[i].durationOfLIfe <= 6 && individuals._names[i].durationOfLIfe >= 4)  {
             individuals._names[i].gender === 'male' ? msix++ : fsix++;
+            kcount++;
         } else if (individuals._names[i].durationOfLIfe <= 12 && individuals._names[i].durationOfLIfe >= 6)   {
             individuals._names[i].gender === 'male' ? mtwelve++ : ftwelve++;
+            kcount++;
         } else if (individuals._names[i].durationOfLIfe <= 16 && individuals._names[i].durationOfLIfe >= 12)   {
             individuals._names[i].gender === 'male' ? msixteen++ : fsixteen++;
+           
         }
            
     }
@@ -102,7 +105,7 @@ const individuals = {
           let words = `${ffour} females killed and ${mfour} males killed within less than four weeks. ${fsix} females and ${msix} males killed within four to six
                   weeks. ${ftwelve} females killed and ${mtwelve} males killed within six to twelve weeks. ${fsixteen} females might survive longer and ${msixteen} 
                   males might survive longer over a period of eighteen years.`
-        wordsOne = `<p>Assuming the team of corrupt federal police officers constantly harrass individuals. Over a duration of eighteen years, a theorized team of corrupt federal police officers could manage to kill via harrassment ${ffour+mfour} people who last less than four weeks. ${msix+fsix} people who last between four and six weeks ${mtwelve+ftwelve } people who live between six and twelve weeks or three months which leaves ${msixteen+fsixteen} people alive whom live for a duration of no less than twelve to sixteen weeks of constant harrassment. If they survive this duratoin they may or may not live for much longer durations. All up ${mcount+fcount} were harrassed and ${(mfour + msix + mtwelve+ffour + fsix + ftwelve)} people were killed.</p>`;
+        wordsOne = `<p>Assuming the team of corrupt federal police officers constantly harrass individuals. Over a duration of eighteen years, a theorized team of corrupt federal police officers could manage to kill via harrassment ${ffour+mfour} people who last less than four weeks. ${msix+fsix} people who last between four and six weeks ${mtwelve+ftwelve } people who live between six and twelve weeks or three months which leaves ${msixteen+fsixteen} people alive whom live for a duration of no less than twelve to sixteen weeks of constant harrassment. If they survive this duratoin they may or may not live for much longer durations. All up ${individuals._names.length} were harrassed and ${kcount} people were killed.</p>`;
 
        
         let list = ''
@@ -133,3 +136,4 @@ const individuals = {
         $information.append($container);
       });    
 })();
+
